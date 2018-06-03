7b48e283a78dc3975a1988220232b37cacec27fc
kanty <kan4227@gmail.com>
2018-06-03 17:39:38 +0700

    ปรับหน้า form กรอกข้อมูลของ user & admin
    เอาหัวข้อฟอร์มออกจาก <form>

--- admin_login.php
+++ admin_login.php
@@ -1,16 +1,18 @@
 <div class="row">
-    <form class="col s4 offset-s4" action="?location=action/admin_check.php" method="post">
-        <h4 class="header-content">เข้าสู่ระบบผู้ดูแล</h4>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="uname" name="username" type="text" class="validate" required>
-                <label for="uname">ฃื่อผู้ใช้งาน</label>
+    <h4 class="header-content">เข้าสู่ระบบผู้ดูแล</h4>
+    <div class="col s6 offset-s3">
+        <form action="?location=action/admin_check.php" method="post">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="uname" name="username" type="text" class="validate" required>
+                    <label for="uname">ฃื่อผู้ใช้งาน</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="pwd" name="password" type="password" class="validate" required>
+                    <label for="pwd">รหัสผ่าน</label>
+                </div>
+                <button id="submit" type="submit" class="light-blue darken-2 waves-effect waves-light btn">ยืนยัน</button>
             </div>
-            <div class="input-field col s12">
-                <input id="pwd" name="password" type="password" class="validate" required>
-                <label for="pwd">รหัสผ่าน</label>
-            </div>
-            <button id="submit" type="submit" class="light-blue darken-2 waves-effect waves-light btn">ยืนยัน</button>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
--- form.php
+++ form.php
@@ -1,27 +1,29 @@
 <div class="row">
-    <form class="col s4 offset-s4" action="?location=action/set_session.php" method="post">
-        <h4 class="header-content">กรอกข้อมูลก่อนทำแบบทดสอบ</h4>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="id" name="id" type="text" class="validate" required>
-                <label for="id">รหัสพนักงาน</label>
-            </div>
-            
-            <div class="input-field col s12">
-                <input id="name" name="name" type="text" class="validate" required>
-                <label for="name">ชื่อ - นามสกุล</label>
-            </div>
-            
-            <div class="input-field col s12">
-                <select name="department" required>
-                    <option value="" disabled selected>กรุณาเลือกแผนก</option>
-                    <?php for($i = 0; $i < sizeof($department);$i++){
+    <h4 class="header-content">กรอกข้อมูลก่อนทำแบบทดสอบ</h4>
+    <div class="col s6 offset-s3">
+        <form action="?location=action/set_session.php" method="post">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="id" name="id" type="text" class="validate" required>
+                    <label for="id">รหัสพนักงาน</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <input id="name" name="name" type="text" class="validate" required>
+                    <label for="name">ชื่อ - นามสกุล</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <select name="department" required>
+                        <option value="" disabled selected>กรุณาเลือกแผนก</option>
+                        <?php for($i = 0; $i < sizeof($department);$i++){
                     echo "<option value='$department[$i]'>$department[$i]</option>";
                      } ?>
-                </select>
-                <label>แผนก</label>
+                    </select>
+                    <label>แผนก</label>
+                </div>
+                <button id="submit" type="submit" class="light-blue darken-2 waves-effect waves-light btn">ยืนยัน</button>
             </div>
-            <button id="submit" type="submit" class="light-blue darken-2 waves-effect waves-light btn">ยืนยัน</button>
-        </div>
-    </form>
+        </form>
+    </div>
 </div>
